5eb9e813a1671cc20951ab424900ae1a91632a5c
DavidJeonKr <dudgh7410@naver.com>
2022-07-04 23:40:55 +0900

    remote .git files

---
+++ give-front-end/src/App.js
@@ -0,0 +1,20 @@
+// import './css/App.css';
+import axios from "axios";
+import React, {useEffect} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Header from "./components/Header";
+
+
+function App() {
+
+    return (
+        <BrowserRouter>
+            <Header/>
+
+
+        </BrowserRouter>
+
+    );
+}
+
+export default App;
---
+++ give-front-end/src/components/Header.js
@@ -0,0 +1,12 @@
+
+
+export default function Header() {
+
+
+    return (
+        <div>
+            <p>여기는 헤더</p>
+        </div>
+    );
+}
+
